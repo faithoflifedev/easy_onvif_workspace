4d41ffe15c286a04bd269fa7cddfa858d5fb25cb
Chris D <chris@venngo.com>
2021-10-22 11:10:11 -0400

    ONVIF SOAP faults do not match the spec they refer to on some devices, and now a static instance of Dio

--- lib/model/detail.dart
+++ lib/model/detail.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:json_annotation/json_annotation.dart';
 
 import 'i8nText.dart';
@@ -9,11 +11,14 @@ class Detail {
   @JsonKey(name: 'Text')
   final I8nText i8nText;
 
-  String get text => i8nText.text;
+  String? get text => i8nText.text;
 
-  Detail(this.i8nText);
+  Detail({required this.i8nText});
 
   factory Detail.fromJson(Map<String, dynamic> json) => _$DetailFromJson(json);
 
   Map<String, dynamic> toJson() => _$DetailToJson(this);
+
+  @override
+  String toString() => json.encode(toJson());
 }
--- lib/model/fault.g.dart
+++ lib/model/fault.g.dart
@@ -7,17 +7,21 @@ part of 'fault.dart';
 // **************************************************************************
 
 Fault _$FaultFromJson(Map<String, dynamic> json) => Fault(
-      Code.fromJson(json['Code'] as Map<String, dynamic>),
-      Reason.fromJson(json['Reason'] as Map<String, dynamic>),
-      json['Node'],
-      json['Role'],
-      Detail.fromJson(json['Detail'] as Map<String, dynamic>),
+      code: Code.fromJson(json['Code'] as Map<String, dynamic>),
+      reason: json['Reason'] == null
+          ? null
+          : Reason.fromJson(json['Reason'] as Map<String, dynamic>),
+      xmlNode: json['Node'],
+      xmlRole: json['Role'],
+      detail: json['Detail'] == null
+          ? null
+          : Detail.fromJson(json['Detail'] as Map<String, dynamic>),
     );
 
 Map<String, dynamic> _$FaultToJson(Fault instance) => <String, dynamic>{
       'Code': instance.code.toJson(),
-      'Reason': instance.reason.toJson(),
+      'Reason': instance.reason?.toJson(),
       'Node': instance.xmlNode,
       'Role': instance.xmlRole,
-      'Detail': instance.detail.toJson(),
+      'Detail': instance.detail?.toJson(),
     };
